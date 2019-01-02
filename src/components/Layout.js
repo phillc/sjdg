77fec87180e247b5fc892ea632fb37ae9a93b2a6
phillc <spyyderz@gmail.com>
2019-01-02 07:22:15 -0500

    Annnddddd back to blank

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,46 +1,44 @@
 import React from 'react'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from "gatsby"
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
 
-import Navbar from '../components/Navbar'
-import './all.sass'
+import Header from './header'
+import './layout.css'
 
-const TemplateWrapper = ({ children }) => (
+const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
-      query HeadingQuery {
-          site {
-            siteMetadata {
-              title,
-              description,
-            }
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
           }
         }
+      }
     `}
     render={data => (
-      <div>
-        <Helmet>
-          <html lang="en" />
-          <title>{data.site.siteMetadata.title}</title>
-          <meta name="description" content={data.site.siteMetadata.description} />
-          
-          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
-	        <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
-	        <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
-	
-	        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
-	        <meta name="theme-color" content="#fff" />
-
-	        <meta property="og:type" content="business.business" />
-          <meta property="og:title" content={data.site.siteMetadata.title} />
-          <meta property="og:url" content="/" />
-          <meta property="og:image" content="/img/og-image.jpg" />
-        </Helmet>
-        <Navbar />
-        <div>{children}</div>
-      </div>
+      <>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          {children}
+          <footer>
+            © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
+      </>
     )}
   />
 )
 
-export default TemplateWrapper
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
