1dad13d871889849ae38846ff21a9fd2b849e330
phillc <spyyderz@gmail.com>
2019-03-27 18:29:37 -0400

    Fix responsive menu

--- src/components/header-carousel.js
+++ src/components/header-carousel.js
@@ -1,45 +1,48 @@
-import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 import Carousel from 'nuka-carousel';
-import Img from "gatsby-image"
+import Img from "gatsby-image";
 
 const HeaderCarousel = () => {
-  return <div>
-    <div className="header-carousel-overlay" />
-    <div className="header-carousel">
-      <StaticQuery
-        query={graphql`
-          query {
-            images: allFile(sort: {fields: [name], order: ASC}, filter: { relativeDirectory: {eq: "front"} }) {
-              edges {
-                node {
-                  childImageSharp {
-                    fluid(maxWidth: 1280) {
-                      ...GatsbyImageSharpFluid
+  return (
+    <div>
+      <div className="header-carousel-overlay" />
+      <div className="header-carousel">
+        <StaticQuery
+          query={graphql`
+            query {
+              images: allFile(sort: {fields: [name], order: ASC}, filter: { relativeDirectory: {eq: "front"} }) {
+                edges {
+                  node {
+                    childImageSharp {
+                      fluid(maxWidth: 1280) {
+                        ...GatsbyImageSharpFluid
+                      }
                     }
                   }
                 }
               }
             }
-          }
-        `}
-        render={data => (
-          <Carousel
-            autoplay={true}
-            autoplayInterval={10000}
-            pauseOnHover={false}
-            withoutControls={true}
-            wrapAround={true}
-            autoGenerateStyleTag={false}
-            slideIndex={0}
-          >
-            {data.images.edges.map((edge, i) =>
-              <Img key={i} fluid={edge.node.childImageSharp.fluid} style={{height: "100vh"}} />
-            )}
-          </Carousel>
-        )}
-      />
+          `}
+          render={data => (
+            <Carousel
+              autoplay={true}
+              autoplayInterval={10000}
+              pauseOnHover={false}
+              withoutControls={true}
+              wrapAround={true}
+              autoGenerateStyleTag={false}
+              slideIndex={0}
+            >
+              {data.images.edges.map((edge, i) =>
+                                     <Img key={i} fluid={edge.node.childImageSharp.fluid} style={{height: "100vh"}} />
+                                    )}
+            </Carousel>
+          )}
+        />
+      </div>
     </div>
-  </div>
-}
-export default HeaderCarousel
+  );
+};
+
+export default HeaderCarousel;
--- src/components/header-navigation.js
+++ src/components/header-navigation.js
@@ -1,58 +1,78 @@
-import { StaticQuery, graphql } from 'gatsby'
-import React from 'react'
-import { Link } from 'gatsby'
+import { StaticQuery, graphql } from 'gatsby';
+import React from 'react';
+import { Link } from 'gatsby';
+import classNames from'classnames';
 
 const NavBarLink = ({to, children}) => {
-	return <Link to={to}
-							 className="header-link navbar-item"
-							 activeClassName="header-link-active">
-		{children}
-	</Link>
+  return (
+    <Link
+      to={to}
+      className="header-link navbar-item"
+      activeClassName="header-link-active">
+      {children}
+    </Link>
+  );
+};
+
+
+
+export default class HeaderNavigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { burgerActive: false };
+
+    this.handleBurgerClick = this.handleBurgerClick.bind(this);
+  }
+
+  handleBurgerClick() {
+    this.setState(state => ({
+      burgerActive: !state.burgerActive
+    }));
+  }
+
+  render() {
+    return (
+      <div className="header-navigation hero-head">
+        <header className="navbar">
+          <div className="container">
+            <div className="navbar-brand">
+              <Link to="/" className="navbar-item">
+                <StaticQuery
+                  query={graphql`
+                  query {
+                    logo: file(relativePath: { eq: "logo.png" }) {
+                      childImageSharp {
+                        fixed(height: 100) {
+                          ...GatsbyImageSharpFixed
+                        }
+                      }
+                    }
+                  }
+                `}
+                  render={data => (
+                    <img src={data.logo.childImageSharp.fixed.src} className="header-navigation-logo" alt="SJDG" />
+                  )}
+                />
+                <h1>South Jersey Disc Golf</h1>
+              </Link>
+              <span className={classNames("navbar-burger burger", { "is-active": this.state.burgerActive })} onClick={this.handleBurgerClick}>
+                <span></span>
+                <span></span>
+                <span></span>
+              </span>
+            </div>
+            <div className={classNames("navbar-menu", { "is-active": this.state.burgerActive })}>
+              <div className="navbar-end">
+                <NavBarLink to="/">Home</NavBarLink>
+                {/*<NavBarLink to="/about">About</NavBarLink>*/}
+                {/*<NavBarLink to="/courses">Courses</NavBarLink>*/}
+                {/*<NavBarLink to="/events">Events</NavBarLink>*/}
+                {/*<NavBarLink to="/contact">Contact</NavBarLink>*/}
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+  };
 }
-
-
-const HeaderNavigation = () => {
-	return <div className="header-navigation hero-head">
-		<header className="navbar">
-			<div className="container">
-				<div className="navbar-brand">
-					<Link to="/" className="navbar-item">
-						<StaticQuery
-							query={graphql`
-								query {
-									logo: file(relativePath: { eq: "logo.png" }) {
-										childImageSharp {
-											fixed(height: 100) {
-												...GatsbyImageSharpFixed
-											}
-										}
-									}
-								}
-							`}
-							render={data => (
-                <img src={data.logo.childImageSharp.fixed.src} className="header-navigation-logo" alt="SJDG" />
-							)}
-						/>
-						<h1>South Jersey Disc Golf</h1>
-					</Link>
-					<span className="navbar-burger burger" data-target="navbarMenuHeroC">
-						<span></span>
-						<span></span>
-						<span></span>
-					</span>
-				</div>
-				<div id="navbarMenuHeroC" className="navbar-menu is-active">
-					<div className="navbar-end">
-            <NavBarLink to="/">Home</NavBarLink>
-            {/*<NavBarLink to="/about">About</NavBarLink>*/}
-            {/*<NavBarLink to="/courses">Courses</NavBarLink>*/}
-						{/*<NavBarLink to="/events">Events</NavBarLink>*/}
-						{/*<NavBarLink to="/contact">Contact</NavBarLink>*/}
-					</div>
-				</div>
-			</div>
-		</header>
-	</div>
-}
-
-export default HeaderNavigation
